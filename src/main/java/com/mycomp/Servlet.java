e2a08d93c2459c0cd431f883a2c81206550bb42c
Taptigin <trpk30273@gmail.com>
2016-07-18 14:58:50 +0400

    beginning hibernate

--- src/main/java/com/mycomp/Servlet.java
+++ src/main/java/com/mycomp/Servlet.java
@@ -1,11 +1,14 @@
 package com.mycomp;
 
+import org.hibernate.Session;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Александр on 18.07.2016.
@@ -13,6 +16,10 @@ import java.io.IOException;
 @WebServlet(name = "Servlet", urlPatterns = {"/Servlet"})
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<UsersEntity> list = null;
+
 
         request.getRequestDispatcher("/index.jsp").forward(request,response);
     }
