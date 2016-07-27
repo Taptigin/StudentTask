1e40e9802d636fd4e309fb6b661d7991e860bbc7
Taptigin <trpk30273@gmail.com>
2016-07-27 13:18:54 +0400

    123

---
+++ src/main/java/com/mycomp/client/GwtTable.java
@@ -0,0 +1,12 @@
+package com.mycomp.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+/**
+ * Created by Александр on 27.07.2016.
+ */
+public class GwtTable implements EntryPoint {
+    public void onModuleLoad() {
+    }
+}
