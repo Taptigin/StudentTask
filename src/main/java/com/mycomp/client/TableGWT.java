2566dd8b604f5a851e9c82e10ae2cae592cad690
Taptigin <trpk30273@gmail.com>
2016-08-01 11:40:45 +0400

    Рабочая заготовка 2.

--- src/main/java/com/mycomp/client/TableGWT.java
+++ src/main/java/com/mycomp/client/TableGWT.java
@@ -2,6 +2,8 @@ package com.mycomp.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.*;
@@ -13,55 +15,15 @@ import java.util.List;
  */
 public class TableGWT implements EntryPoint {
 
-    private String s;
-    private final FlexTable t = new FlexTable();
+    DataGrid<UsersEntity> table = new DataGrid<UsersEntity>();
 
 
     public void onModuleLoad() {
 
-        TableServiceAsync swc = GWT.create(TableService.class);
-
-        AsyncCallback<List<UsersEntity>> callback = new AsyncCallback<List<UsersEntity>>() {
-            @Override
-            public void onFailure(Throwable caught) {
-                Window.alert("Fail getAllUsers");
-            }
-
-            @Override
-            public void onSuccess(List<UsersEntity> result) {
-
-                s = result.get(1).toString();
-                Window.alert(s);
-
-                for (int i = 0; i < 250; i++) {
-                    t.setText(i,1,result.get(i).getFirstName());
-                    t.setText(i,2,result.get(i).getLastName());
-                    t.setText(i,3,result.get(i).getSex());
-                    t.setText(i,4,result.get(i).getEnrollmentDate().toString());
-
-                }
-
-
-            }
-        };
-
-        swc.getAll(callback);
-
-        // Put some text at the table's extremes.  This forces the table to be
-        // 3 by 3.
-        t.setText(0, 0, "upper-left corner");
-        t.setText(2, 2, "bottom-right corner");
-
-        // Let's put a button in the middle...
-        t.setWidget(1, 0, new Button("Wide Button"));
-
-        // ...and set it's column span so that it takes up the whole row.
-        t.getFlexCellFormatter().setColSpan(1, 0, 3);
+        table.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.ENABLED);
 
 
 
-        t.setText(2,2,s);
-        RootPanel.get().add(t);
 
     }
 
