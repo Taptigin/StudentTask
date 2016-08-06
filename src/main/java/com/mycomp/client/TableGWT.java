6ff72d1761490848d4bc07945c3a7f44e1c47fb5
Taptigin <trpk30273@gmail.com>
2016-08-06 23:39:23 +0400

    доработка фронта1

--- src/main/java/com/mycomp/client/TableGWT.java
+++ src/main/java/com/mycomp/client/TableGWT.java
@@ -4,7 +4,6 @@ import com.google.gwt.cell.client.DateCell;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.layout.client.Layout;
 import com.google.gwt.user.cellview.client.*;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -12,10 +11,9 @@ import com.google.gwt.user.client.ui.*;
 import com.google.gwt.view.client.*;
 import com.mycomp.shared.UsersEntity;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import static com.google.gwt.dom.client.Style.Unit.PCT;
 
 /**
  * Created by Александр on 28.07.2016.
@@ -25,102 +23,111 @@ public class TableGWT implements EntryPoint {
     DataGrid<UsersEntity> table = new DataGrid<UsersEntity>();
     SimplePager pager;
     Long rowCount;
-    List<UsersEntity> list;
-    int counter;
+
+    TableServiceAsync swc = GWT.create(TableService.class);
+    List<UsersEntity> list = new ArrayList<>();
 
 
 
-    public void onModuleLoad() {
-
-        table.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.ENABLED);
-        table.setAutoHeaderRefreshDisabled(true);
-        TableServiceAsync swc = GWT.create(TableService.class);
+    private List<UsersEntity> getData(int firstId,int lastId){
 
         AsyncCallback<List<UsersEntity>> callback = new AsyncCallback<List<UsersEntity>>() {
             @Override
             public void onFailure(Throwable caught) {
                 Window.alert("Сервис не запустился.");
-
             }
 
             @Override
             public void onSuccess(List<UsersEntity> result) {
-
-
-                list = result;
-
-                SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
-                pager = new SimplePager(SimplePager.TextLocation.CENTER, pagerResources, true, 0, true);
-
-
-                table.addColumn(getFirstName(),"Имя");
-                table.addColumn(getMiddleName(),"Отчество");
-                table.addColumn(getLastName(),"Фамилия");
-                table.addColumn(getSex(),"Пол");
-                table.addColumn(getEnrollmentDate(),"Дата поступления");
-                table.addColumn(getReleaseDate(),"Дата окончания");
-
-                updateTable();
-
-                table.setRowCount(rowCount.intValue(), true);
-
-                table.addRangeChangeHandler(new RangeChangeEvent.Handler() {
-                    @Override
-                    public void onRangeChange(RangeChangeEvent event) {
-                        updateTable();
-
-
-                    }
-                });
-                table.setWidth("100%");
-
-                pager.setDisplay(table);
-                pager.setPageSize(50);
-
-                DockLayoutPanel panel = new DockLayoutPanel(Style.Unit.PX);
-
-                panel.addNorth(new HTMLPanel("h1", "Список студентов"), 60);
-                panel.addNorth(table,500);
-                panel.add(pager);
-
-
-
-
-
-
-                RootLayoutPanel.get().add(panel);
-
-
-               // Window.alert(result.get(5).toString());
-
+                 list = result; Window.alert(list.get(2).toString());
             }
         };
+        swc.getAll(firstId,lastId,callback);
+        return list;
+    }
 
-        AsyncCallback<Long> asyncCallback = new AsyncCallback<Long>() {
+    private void createTable(){
+        table.setKeyboardSelectionPolicy(HasKeyboardSelectionPolicy.KeyboardSelectionPolicy.ENABLED);
+
+        table.addColumn(getFirstName(),"Имя");
+        table.addColumn(getMiddleName(),"Отчество");
+        table.addColumn(getLastName(),"Фамилия");
+        table.addColumn(getSex(),"Пол");
+        table.addColumn(getEnrollmentDate(),"Дата поступления");
+        table.addColumn(getReleaseDate(),"Дата окончания");
+
+        table.setAutoHeaderRefreshDisabled(true);
+
+        table.setRowCount(rowCount.intValue(), true);
+        table.setWidth("100%");
+        updateTable(1,50);
+
+
+        table.addRangeChangeHandler(new RangeChangeEvent.Handler() {
             @Override
-            public void onFailure(Throwable caught) {
-                Window.alert("Не сработало возвращение RowCount");
+            public void onRangeChange(RangeChangeEvent event) {
+                updateTable(pager.getDisplay().getVisibleRange().getStart(),
+                        pager.getDisplay().getVisibleRange().getLength()-pager.getDisplay().getVisibleRange().getStart());
             }
-
-            @Override
-            public void onSuccess(Long result) {
-                rowCount = result;
-
-            }
-        };
-
-
-        swc.getAll(1,200,callback);
-        swc.getRowCount(asyncCallback);
-
-
-
-
-
-
+        });
 
 
     }
+
+    private void createPager(){
+        SimplePager.Resources pagerResources = GWT.create(SimplePager.Resources.class);
+        pager = new SimplePager(SimplePager.TextLocation.CENTER, pagerResources, true, 0, true);
+
+        pager.setDisplay(table);
+        pager.setPageSize(50);
+    }
+
+    public void onModuleLoad() {
+
+        getRowCount(); Window.alert("rowCount sucsess");
+
+        createTable(); Window.alert("Table created");
+        createPager(); Window.alert("pager created");
+
+        DockLayoutPanel panel = new DockLayoutPanel(Style.Unit.PX);
+
+        panel.addNorth(new HTMLPanel("h1", "Список студентов"), 60);
+        panel.addNorth(table,500);
+        panel.add(pager);
+
+        RootLayoutPanel.get().add(panel);
+
+
+
+            }
+
+
+        private void getRowCount(){
+            AsyncCallback<Long> asyncCallback = new AsyncCallback<Long>() {
+                @Override
+                public void onFailure(Throwable caught) {
+                    Window.alert("Не сработало возвращение RowCount");
+                }
+
+                @Override
+                public void onSuccess(Long result) {
+                    rowCount = result;
+
+                }
+            };
+
+
+            swc.getRowCount(asyncCallback);
+        }
+
+
+
+
+
+
+
+
+
 
     private TextColumn<UsersEntity> getLastName(){
         TextColumn<UsersEntity> lastName = new TextColumn<UsersEntity>() {
@@ -184,10 +191,14 @@ public class TableGWT implements EntryPoint {
         return releaseDate;
     }
 
-    private void updateTable(){
-        table.setRowData(0,list);
+    private void updateTable(int firstId, int lastId){
+
+        Window.alert("В таблицу пришел FIRST = " + firstId + "LAST = " + lastId);
+        table.setRowData(0,getData(firstId, lastId));
+
 
     }
+
 
 
 
