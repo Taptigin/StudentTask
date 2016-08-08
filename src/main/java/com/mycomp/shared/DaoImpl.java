4b50f933d9a1704d6e0b4ad1a1bdfc0da5012dd1
Taptigin <trpk30273@gmail.com>
2016-08-08 13:54:49 +0400

    перевод DAO на критериан

--- src/main/java/com/mycomp/shared/DaoImpl.java
+++ src/main/java/com/mycomp/shared/DaoImpl.java
@@ -3,6 +3,7 @@ package com.mycomp.shared;
 
 import org.springframework.stereotype.Repository;
 
+import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -15,6 +16,7 @@ import java.util.List;
 /**
  * Created by Александр on 20.07.2016.
  */
+@Singleton
 @Repository
 public class DaoImpl implements DaoInterface {
 
@@ -26,12 +28,6 @@ public class DaoImpl implements DaoInterface {
 
     @Override
     public List<UsersEntity> getAll(int firstId, int lastId) {
-
-//        Query query = em.createQuery("from UsersEntity u where u.id >= :firstId and id<= :lastID", UsersEntity.class);
-//        query.setParameter("firstId", firstId);
-//        query.setParameter("lastID", lastId);
-//
-//        List<UsersEntity> list = query.getResultList();
 
         CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
         CriteriaQuery<UsersEntity> query = criteriaBuilder.createQuery(UsersEntity.class);
